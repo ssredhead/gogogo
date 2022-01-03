88bfb5db6030edcd77f73a55f6798201293c731d
Sophia Sommers <soommers.sophia@gmail.com>
2022-01-02 19:07:49 -0500

    fmt print

--- project_1_comic_mischief.go
+++ project_1_comic_mischief.go
@@ -6,4 +6,6 @@ func main() {
   var publisher, writer, artist, title string
   var year, pageNumber int8
   var grade float32
+
+  fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "in", year, "for grade", grade, "with", pageNumber, "pages")
 }
