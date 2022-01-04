25f73820e5bbe787b464b1b3d252ae760dc9d466
Sophia Sommers <soommers.sophia@gmail.com>
2022-01-03 19:56:01 -0500

    variables

--- project_1_comic_mischief.go
+++ project_1_comic_mischief.go
@@ -7,5 +7,13 @@ func main() {
   var year, pageNumber int8
   var grade float32
 
+  title = "Mr. GoToSleep"
+  writer = "Tracy Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+
   fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "in", year, "for grade", grade, "with", pageNumber, "pages")
 }
