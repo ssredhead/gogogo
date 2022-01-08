68116aac33fb8f9f3fd5a1b4d787a13187615e38
Sophia Sommers <soommers.sophia@gmail.com>
2022-01-07 19:24:09 -0500

    formatting variables in print

---
+++ printf.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  animal1 := "cat"
+  animal2 := "dog"
+  
+  // Printf() and the verb placeholder "%v"
+  fmt.Printf("Are you a %v or a %v person?", animal1, animal2)
+  
+}
