09a1f2195f99cc20428b9103a6f483bd731bd3a8
Sophia Sommers <soommers.sophia@gmail.com>
2022-01-15 17:48:47 -0500

    greater than and less than

---
+++ comparison_operators_2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	vaultAmt := 2356468
+	
+  //Comparison operators:
+
+  if vaultAmt >= 200000 {
+    fmt.Println("We're going to need more bags.")
+  }
+  
+}
