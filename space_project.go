3723cd5dba46bd519509fb5b34aa2f77b1d4f10e
Sophia Sommers <soommers.sophia@gmail.com>
2022-02-04 21:09:01 -0500

    functions and such

---
+++ space_project.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+func fuelGauge(fuel int) {
+  fmt.Println("You have", fuel, "gallons of fuel left!")
+}
+
+// Create the function calculateFuel() here
+
+
+// Create the function greetPlanet() here
+
+
+// Create the function cantFly() here
+
+
+// Create the function flyToPlanet() here
+
+
+func main() {
+  // Test your functions!
+  
+  // Create `planetChoice` and `fuel`
+  
+  // And then liftoff!
+  
+}
