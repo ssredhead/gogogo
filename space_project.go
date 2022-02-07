fb1ba901c3abe257d4700c1f8b7628b2288f4144
Sophia Sommers <soommers.sophia@gmail.com>
2022-02-06 20:14:46 -0500

    continued work

--- space_project.go
+++ space_project.go
@@ -10,6 +10,21 @@ func fuelGauge(fuel int) {
 // Create the function calculateFuel() here
 func calculateFuel(planet string) int {
   var fuel int
+  switch planet {
+    case "Venus": {
+      fuel = 300000
+    }
+    case "Mercury": {
+      fuel = 500000
+    }
+    case "Mars": {
+      fuel = 700000
+    }
+    default: {
+      fuel = 0
+    }
+  }
+  return fuel
 }
 
 // Create the function greetPlanet() here
