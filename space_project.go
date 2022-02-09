6e1b2ae7e6d021e957aa05fb740ef9b77fc22c3f
Sophia Sommers <soommers.sophia@gmail.com>
2022-02-08 19:36:19 -0500

    more functions

--- space_project.go
+++ space_project.go
@@ -33,7 +33,9 @@ func greetPlanet(planet string) {
 }
 
 // Create the function cantFly() here
-
+func cantFly() {
+  fmt.Println("We do not have the available fuel to fly there.")
+}
 
 // Create the function flyToPlanet() here
 
